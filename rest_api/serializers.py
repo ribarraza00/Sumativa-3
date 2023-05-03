19b0080cabf87c30a21f6360dbf295f80fbc53a6
fabianagc <114347456+fabianagc@users.noreply.github.com>
2023-05-02 20:52:30 -0400

    se realizaon comentarios en el serializers

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from app.models import Persona
 
+#personalizacion de Tabla Persona hacia json
 class PersonaSerializers(serializers.ModelSerializer):
     class Meta:
         model = Persona
         fields =['id_per','nombre_per','apellido_per','correo_per','clave_per','ciudad_per','region_per','codPostal_per']
+
 
 class PersonaSerializers2(serializers.ModelSerializer):
     class Meta:
